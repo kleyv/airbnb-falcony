013aba7f75abf94672b379eef7d2986b3961bade
Raffaele Viggiani <raffaele.viggiani@gmail.com>
2020-08-25 18:51:16 +0200

    Tuned devise and pages/home and navbar
    We tuned all together this stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  root to: 'projects#index'
+  devise_for :users
+
+  root to: 'pages#home'
   resources :projects, only: [ :index, :show ]
 end
