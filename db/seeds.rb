269b717b261464e2b4bab2a556a0d7c1af77022a
Eukleyv Cardoso <eukleyvcardoso@gmail.com>
2020-08-26 14:08:34 +0100

    Improve seeds to have more investments per investor
    I changed the algorithm to have more investors and so that each
    investor can have more than one investment and bookmark per
    project.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.destroy_all
 
 # Add 10 users
 # Add 5 investors
-puts 'Generatiing investors'
+puts 'Generating investors'
 5.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
@@ -32,8 +32,8 @@ end
 puts 'Investors done!'
 
 # Add 5 owners
-puts 'Generatiing owners'
-5.times do
+puts 'Generating owners...'
+15.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   username = "#{first_name.downcase}#{last_name}"
@@ -50,38 +50,48 @@ end
 puts 'Owners done!'
 
 # Add 5 projects
-puts 'Generatiing projects'
+puts 'Generating projects...'
 User.where(role: 'owner').each do
-  Project.create!(
-    name: "#{Faker::Movies::HarryPotter.spell} #{Faker::Food.fruits}",
-    total_funding: (50..10_000).to_a.sample,
-    total_shares: (5..100).to_a.sample,
-    open?: [true, false].sample,
-    category: %w( health finance nature technology).sample,
-    owner_id: User.where(role: 'owner').sample.id
-  )
+  rand(1..4).times do
+    Project.create!(
+      name: "#{Faker::Movies::HarryPotter.spell} #{Faker::Food.fruits}",
+      total_funding: (50..10_000).to_a.sample,
+      total_shares: (1..100).to_a.sample,
+      open?: [true, false].sample,
+      category: %w( health finance nature technology).sample,
+      owner_id: User.where(role: 'owner').sample.id
+    )
+  end
 end
 puts 'Projects done!'
 
 # Add 5 fundings
-puts 'Generatiing fundings'
-User.where(role: 'investor').each do
-  Funding.create!(
-    funding_amount: (0..10_000).to_a.sample,
-    funding_shares: (5..100).to_a.sample,
-    project_id: Project.all.sample.id,
-    investor_id: User.where(role: 'investor').sample.id,
-    accepted?: [true, false].sample
+puts 'Generating fundings...'
+User.where(role: 'investor').each do |investor|
+  rounds = (1..Project.count).to_a.sample
+  for i in (1..rounds).to_a do
+    Funding.create!(
+      funding_amount: (1..10_000).to_a.sample,
+      funding_shares: (1..100).to_a.sample,
+      project_id: Project.all[i-1].id,
+      investor_id: investor.id,
+      accepted?: [true, false].sample
     )
+    i -= 1
+  end
 end
 puts 'Fundings done!'
 
 # Add 5 bookmarks
-puts 'Generatiing bookmarks'
-User.where(role: 'investor').each do
-  Bookmark.create!(
-    investor_id: User.where(role: 'investor').sample.id,
-    project_id: Project.all.sample.id
+puts 'Generating bookmarks...'
+User.where(role: 'investor').each do |investor|
+  rounds = (1..Project.count).to_a.sample
+  for i in (1..rounds).to_a do
+    Bookmark.create!(
+      investor_id: investor.id,
+      project_id: Project.all[i-1].id,
     )
+    i -= 1
+  end
 end
 puts 'Bookmarks done!'
