b0391543dd1fa39a1a1e8adf1eb06217ecfc90c4
Florian Ott <florian.ott@gmx.at>
2020-08-26 13:57:50 +0100

    corrected validation

--- app/models/funding.rb
+++ app/models/funding.rb
@@ -2,5 +2,5 @@ class Funding < ApplicationRecord
   belongs_to :project
   belongs_to :investor, :class_name => 'User'
 
-  validates :funding_amount, inclusion: { in: (0..100)}
+  validates :funding_shares, inclusion: { in: (1..100)}
 end
