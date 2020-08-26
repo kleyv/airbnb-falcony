1bbca794fd6cccc44a23df36a7f2096757bfc02a
Tiago Palhoca <palhoca.tiago@gmail.com>
2020-08-26 12:05:01 +0100

    New page forms done
    missing the routing from index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     if @project.save
-      redirect_to project_path(@project)
+      redirect_to index_path
     else
       render :new
     end
