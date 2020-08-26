583b8c20ca0632c8050226b8f9704d0a49a7d887
Tiago Palhoca <palhoca.tiago@gmail.com>
2020-08-26 11:46:26 +0100

    created page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,36 @@
 class ProjectsController < ApplicationController
   # skip_before_action :authenticate_user!, only: :index
+  before_action :set_project, only: [:show]
+
+
+  def new
+   @project = Project.new 
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
 
   def index
     @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+  
+  def project_params
+    params.require(:project).permit(:name, :total_shares, :open?, :total_funding, :category, :owner_id)  
+  end
+
+  def set_project
     @project = Project.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
-  resources :projects, only: [ :index, :show ]
+  resources :projects
 end
