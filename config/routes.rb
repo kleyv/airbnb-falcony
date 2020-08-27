38c932679d91dfa2e0ca9acbefdac74ce02ac595
Raffaele Viggiani <raffaele.viggiani@gmail.com>
2020-08-27 12:06:01 +0200

    Add edit-destroy features
    Links on index, personal and show pages (projects controller)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
-  resources :projects, only: [ :new, :create, :index, :show, :edit, :update ]  do
+  resources :projects do
     collection do
       get :personal
     end
