2b8e8d258e3d8eda47ae8e16721629b66c498fc4
Tiago Palhoca <palhoca.tiago@gmail.com>
2020-08-27 23:30:21 +0100

    Added custom layout for log-in page

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,12 @@ module AirbnbFalcony
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.to_prepare do
+    Devise::SessionsController.layout "log_in_layout"
+    Devise::RegistrationsController.layout "log_in_layout"
+    Devise::ConfirmationsController.layout "log_in_layout"
+    Devise::UnlocksController.layout "log_in_layout"
+    Devise::PasswordsController.layout "log_in_layout"
+end
   end
 end
